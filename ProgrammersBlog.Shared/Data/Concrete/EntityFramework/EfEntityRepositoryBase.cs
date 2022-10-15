25245c9ae7aa5c78b432e99ff02d67fbfba0aae3
Unravel <np_batuQhotmail.com>
2022-10-15 15:27:27 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Shared.Data.Abstract;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<Tentity> : IEntityRepository<Tentity>
+        where Tentity : class, IEntity, new()
+    {
+        private readonly DbContext _context;
+
+        public EfEntityRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Tentity entity)
+        {
+            await _context.Set<Tentity>().AddAsync(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<Tentity, bool>> predicate)
+        {
+            return await _context.Set<Tentity>().AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<Tentity, bool>> predicate)
+        {
+            return await _context.Set<Tentity>().CountAsync(predicate);
+        }
+
+        public async Task DeleteAsync(Tentity entity)
+        {
+            await Task.Run(() => { _context.Set<Tentity>().Remove(entity); });
+        }
+
+        public async Task<IList<Tentity>> GetAllAsync(Expression<Func<Tentity, bool>> predicate = null, params Expression<Func<Tentity, object>>[] includeProperties)
+        {
+            IQueryable<Tentity> query = _context.Set<Tentity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<Tentity> GetAsync(Expression<Func<Tentity, bool>> predicate, params Expression<Func<Tentity, object>>[] includeProperties)
+        {
+            IQueryable<Tentity> query = _context.Set<Tentity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(Tentity entity)
+        {
+            await Task.Run(() => { _context.Set<Tentity>().Update(entity); });
+        }
+    }
+}
