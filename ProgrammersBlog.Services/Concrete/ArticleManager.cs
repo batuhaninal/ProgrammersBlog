2adfa525cda886de66bec65af8d2d3199fff3fb5
Unravel <np_batuQhotmail.com>
2022-11-29 21:23:30 +0300

    Pagination service added.

--- ProgrammersBlog.Services/Concrete/ArticleManager.cs
+++ ProgrammersBlog.Services/Concrete/ArticleManager.cs
@@ -232,5 +232,25 @@ namespace ProgrammersBlog.Services.Concrete
                             : sortedArticles.Take(takeSize.Value).ToList()
             });
         }
+
+        public async Task<IDataResult<ArticleListDto>> GetAllByPagingAsync(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
+        {
+            var articles = categoryId == null 
+                ? await UnitOfWork.Articles.GetAllAsync(a => a.IsActive && !a.IsDeleted, a => a.Category, a => a.User) 
+                : await UnitOfWork.Articles.GetAllAsync(a => a.IsActive && a.CategoryId == categoryId && !a.IsDeleted, a => a.Category, a => a.User);
+
+            var sortedArticles = isAscending
+                ? articles.OrderBy(a => a.Date).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList()
+                : articles.OrderByDescending(a => a.Date).Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+
+            return new DataResult<ArticleListDto>(ResultStatus.Success, new ArticleListDto
+            {
+                Articles = sortedArticles,
+                CategoryId = categoryId == null ? null : categoryId,
+                CurrentPage = currentPage,
+                PageSize = pageSize,
+                TotalCount = articles.Count
+            });
+        }
     }
 }
